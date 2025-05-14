0c86ae02192b7f89b47e738456d3ecc5265b373e
Fredev09 <freddy.castrov@gmail.com>
2025-05-13 21:50:25 -0500

    Commit #4 Avance en la logica del login y registro

---
+++ src/Modelo/Usuario.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author DELL
+ */
+public class Usuario {
+    public String user;
+    public String contra;
+
+    public Usuario(String user, String contra) {
+        this.user = user;
+        this.contra = contra;
+    }
+}
