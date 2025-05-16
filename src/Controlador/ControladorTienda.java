a6148a0c27dd2edd3b26d756875d5f8c13689447
Fredev09 <freddy.castrov@gmail.com>
2025-05-16 08:30:53 -0500

    Commit #13 Avance de las listas

--- src/Controlador/ControladorTienda.java
+++ src/Controlador/ControladorTienda.java
@@ -13,6 +13,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import Modelo.*;
 
 /**
  *
@@ -20,20 +21,91 @@ import javafx.stage.Stage;
  */
 public class ControladorTienda {
 
-    @FXML
-    public Button btnCambiarAProductos;
-    @FXML
-    public Button btnCambiarAProductos2;
-    @FXML
-    public Button btnCambiarAProductos3;
-    @FXML
-   public Button btnCambiarAProductos4;
-    @FXML
-    public Button btnSiguiente1;
-    @FXML
-    public Button btnSiguiente2;
-    @FXML
-    public Button btnSiguiente3;
+    public Nodo<Producto> cabezaCarrito;
+
+    public ControladorTienda ControladorPrincipal;
+
+    public ControladorTienda() {
+        cabezaCarrito = null;
+    }
+
+    public Nodo<Producto> getUltimo() {
+        if (cabezaCarrito == null) {
+            return null;
+        } else {
+            Nodo<Producto> p = cabezaCarrito;
+            while (p.sig != null) {
+                p = p.sig;
+            }
+            return p;
+        }
+    }
+
+    public Nodo<Producto> getAntesNodo(Nodo q) {
+        if (cabezaCarrito == null) {
+            return null;
+        } else {
+            Nodo<Producto> p = cabezaCarrito;
+            while (p.sig != q) {
+                p = p.sig;
+            }
+            return p;
+        }
+    }
+
+    public void añadirCarrito(Producto producto) {
+        Nodo<Producto> nuevoNodo = new Nodo(producto);
+
+        if (ControladorPrincipal != null) {
+            if (ControladorPrincipal.cabezaCarrito == null) {
+                ControladorPrincipal.cabezaCarrito = nuevoNodo;
+            } else {
+                Nodo<Producto> actual = ControladorPrincipal.cabezaCarrito;
+                while (actual.sig != null) {
+                    actual = actual.sig;
+                }
+                actual.sig = nuevoNodo;
+            }
+        } else {
+            if (cabezaCarrito == null) {
+                cabezaCarrito = nuevoNodo;
+            } else {
+                Nodo<Producto> actual = cabezaCarrito;
+                while (actual.sig != null) {
+                    actual = actual.sig;
+                }
+                actual.sig = nuevoNodo;
+            }
+        }
+    }
+
+    public void eliminarDelCarrito(Producto producto) {
+        if (cabezaCarrito == null) {
+   
+        }
+        Nodo<Producto> del = null;
+        Nodo<Producto> p = cabezaCarrito;
+
+        while (p != null) {
+            if (p.dato.equals(producto)) {
+                del = p;
+            }
+            p = p.sig;
+        }
+        if (del == null) {
+            return;
+        }
+        if (del == cabezaCarrito) {
+            cabezaCarrito = cabezaCarrito.sig;
+        } else {
+            Nodo<Producto> anterior = getAntesNodo(del);
+            if (anterior != null) {
+                anterior.sig = del.sig;
+            }
+        }
+        del.sig = null;
+        del.dato = null;
+    }
 
     public void cambiarEscena(ActionEvent event, String fxml) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
@@ -63,4 +135,18 @@ public class ControladorTienda {
     public void cambiarAProductos4(ActionEvent event) throws IOException {
         cambiarEscena(event, "/Vista/Productos_pg4.fxml");
     }
+    @FXML
+    public Button btnCambiarAProductos;
+    @FXML
+    public Button btnCambiarAProductos2;
+    @FXML
+    public Button btnCambiarAProductos3;
+    @FXML
+    public Button btnCambiarAProductos4;
+    @FXML
+    public Button btnSiguiente1;
+    @FXML
+    public Button btnSiguiente2;
+    @FXML
+    public Button btnSiguiente3;
 }
---
+++ src/Modelo/Producto.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+/**
+ *
+ * @author DELL
+ */
+public class Producto {
+
+    public String nombre;
+    public String descripcion;
+    public double precio;
+    public String imagen;
+
+    public Producto(String nombre, String descripcion, double precio, String imagen) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.imagen = imagen;
+    }
+}
+
+
+ 
