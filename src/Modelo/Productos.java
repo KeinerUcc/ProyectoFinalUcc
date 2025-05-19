836f46631cd782361312870985541debdde7911b
KEINER <KEINER@LAPTOP-S8QRHJ16>
2025-05-19 17:30:01 -0500

    Commit #17 Terminamos de crear los objetos de los productos

--- src/Modelo/Productos.java
+++ src/Modelo/Productos.java
@@ -9,93 +9,219 @@ package Modelo;
  * @author KEINER
  */
 public class Productos {
+
     public Producto AnilloRoyalStar = new Producto(
-    "Anillo Royal Star",
-    "Anillo con incrustaciones plateadas en forma de estrella en centro",
-    25000.0,
-    "/ImagenesProductos/Anillo royal star 1.png");
-    
+            "Anillo Royal Star",
+            "Anillo con incrustaciones plateadas en forma de estrella en centro",
+            25000.0,
+            "/ImagenesProductos/Anillo royal star 1.png");
+
     public Producto PulseraCrazy = new Producto(
-    "Pulsera Crazy",
-    "Pulsera dorada con dijes coloridos de sol, luna, fresa y detalles brillantes.",
-    25000.0,
-    "/ImagenesProductos/Pulsera Crazy 2.png");
-    
+            "Pulsera Crazy",
+            "Pulsera dorada con dijes coloridos de sol, luna, fresa y detalles brillantes.",
+            25000.0,
+            "/ImagenesProductos/Pulsera Crazy 2.png");
+
     public Producto AretesNudoGold = new Producto(
-    "Aretes Nudo Gold",
-    "Aretes de nudo dorado son perfectos para quienes buscan una pieza versátil, elegancia y sencillez.",
-    25000.0,
-    "/ImagenesProductos/Aretes nudo gold 3.png");
-    
+            "Aretes Nudo Gold",
+            "Aretes de nudo dorado son perfectos para quienes buscan una pieza versátil, elegancia y sencillez.",
+            25000.0,
+            "/ImagenesProductos/Aretes nudo gold 3.png");
+
     public Producto CadenaItaliana = new Producto(
-    "Cadena Italiana",
-    "Cadena de plata italiana con delicadas esferas, con diseño refinado y acabado brillante",
-    55000.0,
-    "/ImagenesProductos/Cadena Italiana 4.png");
-    
+            "Cadena Italiana",
+            "Cadena de plata italiana con delicadas esferas, con diseño refinado y acabado brillante",
+            55000.0,
+            "/ImagenesProductos/Cadena Italiana 4.png");
+
     public Producto CollarGalaxy = new Producto(
-    "Collar Galaxy",
-    "Collar con cristales en tonos celeste y verde agua, rodeados por órbita dorada, con aleciación en oro.",
-    60000.0,
-    "/ImagenesProductos/Collar galaxy 5.png");
-    
+            "Collar Galaxy",
+            "Collar con cristales en tonos celeste y verde agua, rodeados por órbita dorada, con aleciación en oro.",
+            60000.0,
+            "/ImagenesProductos/Collar galaxy 5.png");
+
     public Producto PulseraVanCleef = new Producto(
-    "Pulsera Van Cleef",
-    "Pulsera en diseño de flores simétricas, en baño de oro de 18k y embellecido con nácar blanco o piedra ónix.",
-    110000.0,
-    "/ImagenesProductos/Pulsera Van cleef 6.png");
-    
+            "Pulsera Van Cleef",
+            "Pulsera en diseño de flores simétricas, en baño de oro de 18k y embellecido con nácar blanco o piedra ónix.",
+            110000.0,
+            "/ImagenesProductos/Pulsera Van cleef 6.png");
+
     public Producto DijeMar = new Producto(
-    "Dije Mar",
-    "Dije de un pequeño velero, en tono dorado con detalles que evocan el mar y la aventura.",
-    25000.0,
-    "/ImagenesProductos/Dije mar 7.png");
-    
+            "Dije Mar",
+            "Dije de un pequeño velero, en tono dorado con detalles que evocan el mar y la aventura.",
+            25000.0,
+            "/ImagenesProductos/Dije mar 7.png");
+
     public Producto RelojConquest = new Producto(
-    "Reloj Conquest",
-    "Reloj Longines Conquest Automatic, un modelo elegante. Presenta una esfera azul metálica.",
-    450000.0,
-    "/ImagenesProductos/Reloj conquest 8.png");
-    
+            "Reloj Conquest",
+            "Reloj Longines Conquest Automatic, un modelo elegante. Presenta una esfera azul metálica.",
+            450000.0,
+            "/ImagenesProductos/Reloj conquest 8.png");
+
     public Producto DijeOsoPanda = new Producto(
-    "Dije Oso Panda",
-    "Dije presenta el rostro de un panda, en tono dorado y cubierto de pequeñas piedras brillantes .",
-    30000.0,
-    "/ImagenesProductos/Dije oso panda 9.png");
-    
+            "Dije Oso Panda",
+            "Dije presenta el rostro de un panda, en tono dorado y cubierto de pequeñas piedras brillantes .",
+            30000.0,
+            "/ImagenesProductos/Dije oso panda 9.png");
+
     public Producto AnilloChaosDouble = new Producto(
-    "Anillo Chaos Double",
-    "Anillo doble ajustable en tono dorado. Uno de los aros tiene diseño trenzado con grabado en X, y el otro está decorado con pequeñas piedras brillantes.",
-    25000.0,
-    "/ImagenesProductos/Anillo chaos double 10.png");
-    
+            "Anillo Chaos Double",
+            "Anillo doble ajustable en tono dorado. Uno de los aros tiene diseño trenzado con grabado en X, y el otro está decorado con pequeñas piedras brillantes.",
+            25000.0,
+            "/ImagenesProductos/Anillo chaos double 10.png");
+
     public Producto CollarFinobolit = new Producto(
-    "Collar Fino Bolit",
-    "Cadena de oro laminado estilo bolitas con diseño sutil, convertido en el complemento ideal. ",
-    250000.0,
-    "/ImagenesProductos/Collar fino bolit 11.png");
-    
+            "Collar Fino Bolit",
+            "Cadena de oro laminado estilo bolitas con diseño sutil, convertido en el complemento ideal. ",
+            250000.0,
+            "/ImagenesProductos/Collar fino bolit 11.png");
+
     public Producto Aretescelestial = new Producto(
-    "Aretes Celestial",
-    "Aretes delicados con estrellas de circonias brillantes y luna dorada, en oro de 24k .",
-    65000.0,
-    "/ImagenesProductos/Aretes celestial 12.png");
-    
+            "Aretes Celestial",
+            "Aretes delicados con estrellas de circonias brillantes y luna dorada, en oro de 24k .",
+            65000.0,
+            "/ImagenesProductos/Aretes celestial 12.png");
+
     public Producto CadenaEsclava = new Producto(
-    "Cadena Esclava",
-    "Cadena con diseño de eslabones con un espacio vacío en su interior, perfecta para el diario .",
-    45000.0,
-    "/ImagenesProductos/Cadena Esclava 13.png");
-    
+            "Cadena Esclava",
+            "Cadena con diseño de eslabones con un espacio vacío en su interior, perfecta para el diario .",
+            45000.0,
+            "/ImagenesProductos/Cadena Esclava 13.png");
+
     public Producto PulseraArrastrada = new Producto(
-    "Pulsera Arrastrada",
-    "Pulsera con baño de oro de 18k, centro en forma de serpiente y detalles en circonia cúbica .",
-    325000.0,
-    "/ImagenesProductos/Pulsera arrastrada 14.png");
-    
+            "Pulsera Arrastrada",
+            "Pulsera con baño de oro de 18k, centro en forma de serpiente y detalles en circonia cúbica .",
+            325000.0,
+            "/ImagenesProductos/Pulsera arrastrada 14.png");
+
     public Producto PulserasCombLuxury = new Producto(
-    "Pulseras Comb Luxury",
-    "Combo de tres pulseras en acero inoxidable, ajustable con diseño sutil y sofisticado.",
-    25000.0,
-    "/ImagenesProductos/Pulseras comb luxury 15.png");
+            "Pulseras Comb Luxury",
+            "Combo de tres pulseras en acero inoxidable, ajustable con diseño sutil y sofisticado.",
+            25000.0,
+            "/ImagenesProductos/Pulseras comb luxury 15.png");
+    public Producto RelojTissot = new Producto(
+            "Reloj Tissot",
+            "Reloj combina la sofisticación suiza con un diseño minimalista y moderno.",
+            320000.0,
+            "/ImagenesProductos/Reloj tissot 16.png");
+
+    public Producto ComboAretesSweet = new Producto(
+            "Combo Aretes Sweet",
+            "Aretes cereza con cristales  forma de corazón, lazo con micro circones y perla clásica.",
+            58000.0,
+            "/ImagenesProductos/Combo Aretes sweet 17.png");
+
+    public Producto DijeOsito = new Producto(
+            "Dije Osito",
+            "Dije en tono dorado y cubierto con pequeñas piedras cristalinas con acabado llamativo.",
+            15000.0,
+            "/ImagenesProductos/Dije osito 18.png");
+
+    public Producto AnilloGoldenLuz = new Producto(
+            "Anillo Golden Luz",
+            "Anillo en acero inoxidable con un delicado baño de oro de 18k, con su acabado refleja la luz con sutileza.",
+            88000.0,
+            "/ImagenesProductos/Anillo Golden luz 19.png");
+
+    public Producto DijeToyota = new Producto(
+            "Dije Toyota",
+            "Dije en tono dorado y en el centro luce el icónico logotipo de Toyota, sofisticado y sutil.",
+            35000.0,
+            "/ImagenesProductos/Dije Toyota 20.png");
+
+    public Producto RelojDolceVita = new Producto(
+            "Reloj DolceVita",
+            "Colección Longines DolceVita, reconocida por su sofisticación. conuna caja rectangular de oro. ",
+            1200000.0,
+            "/ImagenesProductos/Reloj DolceVita 21.png");
+
+    public Producto CadenaGold = new Producto(
+            "Cadena Gold Clásica",
+            "Cadena clásica que nunca pasa de moda con diseño fuerte y elegante en oro laminado de alta calidad.",
+            430000.0,
+            "/ImagenesProductos/Cadena gold 22.png");
+
+    public Producto AnilloGoldenluz = new Producto(
+            "Anillo Golden Nature",
+            "Anillo con un relieve nature alrededor, con incrustaciones y baño de oro de 18k.",
+            75000.0,
+            "/ImagenesProductos/Anillo golden nature 23.png");
+
+    public Producto CollarRama = new Producto(
+            "Collar Rama Dorado",
+            "Collar dorado con una ramita adornada con pequeñas flores blancas y hojas en rosa amatista.",
+            40000.0,
+            "/ImagenesProductos/Collar rama 24.png");
+
+    public Producto AretesCoquet = new Producto(
+            "Aretes Coquet",
+            "Aretes con un cristal facetado envuelto en un aro dorado y en baño de acero. ",
+            20000.0,
+            "/ImagenesProductos/Aretes coquet 25.png");
+
+    public Producto CollarPlataMain = new Producto(
+            "Collar Plata Main",
+            "Collar de pequeñas esferas de plata unidas entre sí,  populares por su aspecto elegante y versátil . ",
+            40000.0,
+            "/ImagenesProductos/Collar plata main 26.png");
+
+    public Producto RelojLadyAutomatic = new Producto(
+            "Reloj Lady Automatic",
+            "Reloj con una caja dorada en acero inoxidable con acabado pulido, que le da un estilo moderno. ",
+            850000.0,
+            "/ImagenesProductos/Reloj Lady Automatic 27.png");
+
+    public Producto CollarCrisFlower = new Producto(
+            "Collar cris flower",
+            "Collar dorado con flores de circonias y detalles circulares, una cadena elegante y luminosa. ",
+            100000.0,
+            "/ImagenesProductos/Collar cris flower 28.png");
+
+    public Producto DijeOsitoenPie = new Producto(
+            "Dije osito en pie",
+            "Dije en tono dorado y cubierto con pequeñas piedras transparentes en baño de acero. ",
+            35000.0,
+            "/ImagenesProductos/Dije osito en pie 29.png");
+
+    public Producto AnilloGreenPow = new Producto(
+            "Anillo green pow",
+            "Anillo central con alas de cristal verde esmeralda. Flanqueado por pequeñas gemas translúcidas. ",
+            45000.0,
+            "/ImagenesProductos/Anillo green pow 30.png");
+
+    public Producto CollarLuzFugaz = new Producto(
+            "Collar luz fugaz",
+            "Collar dorado con un dije central con forma de estrella polar y cristales brillantes en acero. ",
+            55000.0,
+            "/ImagenesProductos/Collar luz fugaz 31.png");
+
+    public Producto AretesBoldHuggies = new Producto(
+            "Aretes bold huggies",
+            "Aretes para las amantes de los accesorios statement, en baño de oro de 24k, con cierre tipo huggie. ",
+            150000.0,
+            "/ImagenesProductos/Aretes bold huggies 32.png");
+
+    public Producto CadenaSingapur = new Producto(
+            "Cadena singapur",
+            "Cadena con diseño entrelazado reflejador de luz en plata laminada de alta calidad. ",
+            110000.0,
+            "/ImagenesProductos/Cadena singapur 33.png");
+
+    public Producto AnilloFlowers = new Producto(
+            "Anillo flowers",
+            "Anillo con cristales brillantes en forma de pétalos, y delicado baño de oro, con acabado simetrico . ",
+            275000.0,
+            "/ImagenesProductos/Anillo flowers 34.png");
+
+    public Producto PulseraDestello = new Producto(
+            "Pulsera destello",
+            "Pulsera con dije colgante de piedra blanca nacarada, en baño de acero inoxidable. ",
+            325000.0,
+            "/ImagenesProductos/Pulsera destello 35.png");
+
+    public Producto CollarPearl = new Producto(
+            "Collar pearl",
+            "Collar con perlas artificiales enlazadas en cadena dorada, con un dije en forma de flor. ",
+            35000.0,
+            "/ImagenesProductos/Collar pearl 36.png");
 }
